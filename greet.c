5e53a2fd970e03f536442188e4d520a9d239e891
Jusmistic <jusmistic@gmail.com>
2021-03-08 16:40:18 +0700

    Create greet.c

---
+++ greet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+// gcc greet.c -o greet -fno-stack-protector -z execstack -no-pie
+
+
+void greet(){
+        char name[100]="";
+        printf("Name locate at: %p\n", name);
+	fflush(stdout);
+        scanf("%s", name);
+        printf("Hello, %s\n", name);
+}
+
+void main(){
+        printf("Who are you?\n");
+        greet();
+	fflush(stdout);
+        printf("Bye :D");
+}
